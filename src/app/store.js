ccb77d1d3066c1991820543f726ea8cc7634ff14
Yohannes sida <yohannestesfayejo@gmail.com>
2023-04-06 17:46:12 +0530

    intial commit

---
+++ src/app/store.js
@@ -0,0 +1,31 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+import { cryptoApi } from '../services/cryptoApi';
+import { cryptoNewsApi } from '../services/cryptoNewsApi';
+
+
+// export default configureStore({
+//   reducer: {
+//     [cryptoApi.reducerPath]: cryptoApi.reducer,
+//     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+//   },
+// });
+
+const store = configureStore({
+  reducer: {
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+  },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(cryptoApi.middleware)
+      .concat(cryptoNewsApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export default store;
+
+ 
